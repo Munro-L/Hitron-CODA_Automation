736b694d8425ac9e7614cf6049e2f42264825590
Munro-L <lindsaymunromail@gmail.com>
2019-02-23 17:39:54 -0500

    Refactored bandwidth_usage.py to use cleaner functions

--- bandwidth_usage.py
+++ bandwidth_usage.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import requests
 import json
-import time
+from time import sleep
 import configparser
 import logging
+
 
 logger = logging.getLogger("bandwidth_use")
 logger.setLevel(logging.DEBUG)
@@ -17,67 +18,75 @@ logger.addHandler(console_handler)
 logger.info("Starting script, polling router for the first time")
 
 config = configparser.ConfigParser()
-config.read("config.config")
-
+config.read("config.txt")
 payload = {"user" : config["CREDS"]["username"], "pws" : config["CREDS"]["password"], "remember_me" : config["CREDS"]["remember_me"]}
 router = config["CREDS"]["router_ip"]
 
 referer_login = "http://{0}/login.html".format(router)
 login_url = "http://{0}/goform/login".format(router)
 
-# test connectivity and log in to router
-while True:
+
+# check connectivity and log in to router
+def connectivity_check():
     session_requests = requests.session()
     result = session_requests.post(login_url, data = payload, headers = dict(referer = referer_login))
-    if result.status_code == 200:
-        logger.info("Got status code 200, router is up")
-        break
-    else:
-        logger.warn("Router is down or script was unable to authenticate, check connectivity")
-        time.sleep(30)
 
-# check router for initial download value
-while True:
-    bandwidth = session_requests.get("http://{0}/data/getSysInfo.asp".format(router))
+    if result.status_code == 200:
+        return session_requests
+    else:
+        return False
+
+
+# get amount downloaded so far this month, and account for metric prefix (put everything in GB)
+def monthly_downloaded(authenticated_session):
+    bandwidth = authenticated_session.get("http://{0}/data/getSysInfo.asp".format(router))
     try:
         parsed = json.loads(bandwidth.content)
         downloaded_old = parsed[0]["WRecPkt"].split(" ", 1)[0]
         if downloaded_old[-1] == "T":
-            float_downloaded_old = float(downloaded_old.replace("T", "")) * 1024
+            float_downloaded = float(downloaded_old.replace("T", "")) * 1024
         elif downloaded_old[-1] == "M":
-            float_downloaded_old = float(downloaded_old.replace("M", "")) / 1024
+            float_downloaded = float(downloaded_old.replace("M", "")) / 1024
         elif downloaded_old[-1] == "G":
-            float_downloaded_old = float(downloaded_old.replace("G", ""))
+            float_downloaded = float(downloaded_old.replace("G", ""))
         else:
-            logger.warn("JSON data from router appears malformed, couldn't find metric prefix")
             raise
-
-        break
+        return float_downloaded
     except:
-        logging.warn("Unable to load JSON data from router")
-        time.sleep(30)
+        return False
 
 
-# main loop to poll router every hour and find difference in amount downloaded
-while True:
-    time.sleep(3600)    # set to be 1 hour, change to whatever you want
-    result = session_requests.post(login_url, data = payload, headers = dict(referer = referer_login))
-    bandwidth = session_requests.get("http://{0}/data/getSysInfo.asp".format(router))
-    try:
-        parsed = json.loads(bandwidth.content)
-        downloaded_new = parsed[0]["WRecPkt"].split(" ", 1)[0]
-        if downloaded_new[-1] == "T":
-            float_downloaded_new = float(downloaded_new.replace("T", "")) * 1024
-        elif downloaded_new[-1] == "M":
-            float_downloaded_new = float(downloaded_new.replace("M", "")) / 1024
-        elif downloaded_new[-1] == "G":
-            float_downloaded_new = float(downloaded_new.replace("G", ""))
+def main():
+    connected = False
+    first_loop = True
+    while True:
+        if not connected:
+            auth_session = connectivity_check()
+            if auth_session:
+                logger.info("Got status code 200, router is up")
+                connected = True
+            else:
+                logger.warn("Router is down or script was unable to authenticate, check connectivity")
+                sleep(30)
+                continue
+
+        downloaded = monthly_downloaded(auth_session)
+        if not downloaded:
+            logging.warn("Unable to load JSON data from router")
+            connected = False
+            continue
+
+        if first_loop:
+            last_downloaded = downloaded
+            first_loop = False
+            sleep(3600)
+            continue
         else:
-            logger.warn("JSON data from router appears malformed, couldn't find metric prefix")
-            raise
-    except:
-        logging.warn("Unable to load JSON data from router")
+            rate = round(downloaded - last_downloaded, 2)
+            logger.info(str(rate))
+            last_downloaded = downloaded
+            sleep(3600)
 
-    rate = round(float_downloaded_new - float_downloaded_old, 2)
-    logger.info(str(rate))
-    float_downloaded_old = float_downloaded_new
+
+if __name__ == "__main__":
+    main()
